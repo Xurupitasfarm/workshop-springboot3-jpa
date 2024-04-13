c83b70442facefb8a98d2cfd94e5d232ac3d8667
Xurupitasfarm <vitornaruto1990@gmail.com>
2024-04-12 23:10:58 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandoweb/course3/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course3.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
